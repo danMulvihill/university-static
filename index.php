aabaa57e9d53505b4f1c10395f9ee4d1e8c813d1
Daniel Mulvihill <dmulvihill3@gmail.com>
2018-12-14 14:54:18 -0500

    Added single.php and other edits

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-This is our custom theme.
+This is our custom theme. Current page: index.php
 
 <h1><?php bloginfo( "name" ) ?></h1>
+<h2><?php bloginfo( "description" ) ?></h2>
+
+<?php
+    while(have_posts()){
+        the_post(); ?>
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <p><?php the_excerpt() ?></p>
+            <hr />
+        <?php
+
+    }
+?>
