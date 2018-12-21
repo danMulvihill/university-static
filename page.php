b8e991e306a63dacc27846b7a97ea72c86fbd67c
Daniel Mulvihill <dmulvihill3@gmail.com>
2018-12-21 11:58:25 -0500

    added content to page.php

---
+++ page.php
@@ -0,0 +1,16 @@
+Current page: page.php 
+
+
+<h1><?php bloginfo( "name" ) ?></h1>
+<h2><?php bloginfo( "description" ) ?></h2>
+
+<?php
+    while(have_posts()){
+        the_post(); ?>
+            <h1><?php the_title() ?></a></h1>
+            <p><?php the_content() ?></p>
+       
+        <?php
+
+    }
+?>
